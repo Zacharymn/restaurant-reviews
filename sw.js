6fc730ebcec73edae9d26559a648ba2d1224ad6c
Zach M <zachdtr@aol.com>
2018-08-01 13:46:08 -0600

    Added Aria Roles/Labels.  Corrected some JS element features H1 to H2 etc.

--- sw.js
+++ sw.js
@@ -4,14 +4,26 @@ self.addEventListener('install', function(event) {
 			return cache.addAll([
 				'/',
 				'index.html',
-				'restaurant.html',
+				'restaurant.html?id=1',
+				'restaurant.html?id=2',
+				'restaurant.html?id=3',
+				'restaurant.html?id=4',
+				'restaurant.html?id=5',
+				'restaurant.html?id=6',
+				'restaurant.html?id=7',
+				'restaurant.html?id=8',
+				'restaurant.html?id=9',
+				'restaurant.html?id=10',
 				'css/responsive.css',
 				'css/styles.css',
 				'js/dbhelper.js',
 				'js/main.js',
 				'js/restaurant_info.js',
 				'data/restaurants.json',
-				'js/sw/index.js'
+				'js/sw/index.js',
+				'sw.js',
+				"https://unpkg.com/leaflet@1.3.1/dist/leaflet.js",
+				"https://unpkg.com/leaflet@1.3.1/dist/leaflet.css"
 			]);
 		})
 	);
